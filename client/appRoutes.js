1cdb5d50ec17038ecff9e2126d9830b0ef40e162
Kartik <kvempati11@gmail.com>
2016-01-07 13:33:47 -0800

    Fix merge conflicts

--- client/appRoutes.js
+++ client/appRoutes.js
@@ -2,10 +2,14 @@ app.config(function($routeProvider, $sceDelegateProvider){
 	$routeProvider
 		.when("/stream", {
 			templateUrl: "stream/stream.html",
-			controller: "streamController",
+			controller: "StreamController"
+		})
+		.when("/login", {
+			templateUrl: "auth/login.html",
+			controller: "AuthController"
 		})
 
-	  $sceDelegateProvider.resourceUrlWhitelist([
+	$sceDelegateProvider.resourceUrlWhitelist([
     // Allow same origin resource loads.
     'self',
     // Allow loading from our assets domain.  Notice the difference between * and **.
--- client/services/services.js
+++ client/services/services.js
@@ -1,3 +1,33 @@
-app.factory('getVideo', function getVideo(url){
+// <<<<<<< 74ec409484f43f9ca920decf110e7dac504c48bd
+// app.factory('getVideo', function getVideo(url){
 	
-})
+// })
+// =======
+angular.module('services', [])
+	.factory('OAuth', function ($http) {
+		var googleLogin = function (data) {	
+			return $http({
+				method: 'POST',
+				url: '/api/oauth/google',
+				// right now, i have no idea what format the data is coming in
+				data: data
+			});
+		}
+
+		return {
+			googleLogin: googleLogin
+		};
+	})
+
+	.factory('getVideo', function () {
+		var videoDetails = function (cb) {
+			var socket = io.connect('http://localhost:8001');
+			socket.on('playerDetails', function (data) {
+				cb(data);
+			})
+		}
+
+		return {
+			videoDetails: videoDetails
+		};
+	})
