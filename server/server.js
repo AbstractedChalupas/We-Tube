b9c45f3bcd7e716b3b19f44aa11f460bf80174a9
Byron Rau <byronrau@gmail.com>
2016-01-07 11:22:34 -0800

    added socket.io

--- server/server.js
+++ server/server.js
@@ -7,13 +7,22 @@ var app = express();
 // Connect to a database called we-tube
 mongoose.connect('mongodb://localhost/we-tube');
 
-
 var PORT = 8001;
 
+var io = require('socket.io').listen(app.listen(PORT));
 
 app.use(express.static(__dirname+"/../client"));
 
+io.on('connection', function (socket) {
+  console.log('client connected');
+  socket.emit('playerDetails', {'videoId': 'bHQqvYy5KYo',
+               'startSeconds': 5,
+               'endSeconds': 60,
+               'suggestedQuality': 'large'});
+  // socket.on('my other event', function (data) {
+  //   console.log(data);
+  // });
+});
 
-app.listen(PORT);
 
 module.exports = app;
