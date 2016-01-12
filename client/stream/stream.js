cd41d26e191725d9390d5d0051e01f8e622e7d08
Kyle Morehead <krmorehead@gmail.com>
2016-01-11 23:43:31 -0800

    rebased and added more comments

--- client/stream/stream.js
+++ client/stream/stream.js
@@ -6,15 +6,17 @@ angular.module('stream', [])
 		//they are kept updated
 		$scope.messages = getVideo.messages;
 		$scope.rooms = [];
+		//gets users from the factory, this could have likely have been done like
+		//the $scope.messages 
 		$scope.user = $rootScope.user.username
 
-
+		//can be used to clear the url field in the submit, not currently in use
 		$scope.clearUrl = function(){
 			$scope.url = ''
 		};
 
+		//gets rooms from the server
 		$scope.getRooms = function(){
-			// $scope.rooms = ['U0315DUM6Cg']
 			console.log("getting rooms")
 			$http({
 				method : "GET",
@@ -26,6 +28,7 @@ angular.module('stream', [])
 			});
 		};
 
+		//sets the videoId from the url provided
 		$scope.submitUrl = function(url){
 			url = url.split("v=")
 			//grabs the youtube video id
@@ -33,6 +36,7 @@ angular.module('stream', [])
 			//pass in true since they are the host
 			getVideo.setupPlayer($scope.videoId, true)			
 		};
+		//loges the user out
 		$scope.logout = function() {
 			return $http({
 				method: "GET",
@@ -46,9 +50,10 @@ angular.module('stream', [])
 		}
 
 		$scope.submitMessage = function(keyCode){
+			//submits message on enter
 			if(keyCode === 13){
 				getVideo.submitMessage($scope.user, $scope.message)
-				// ({user: $scope.user, message:$scope.message})
+				//resets the message field
 				$scope.message = ""
 			}					
 		}
